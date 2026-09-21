a714adf6a583cd9909171d0bcf66ed30cf5cd148
Sumit Kumar <sumitkumarit0098@gmail.com>
2026-09-22 00:24:25 +0530

    updated the path of vault pass from home to jenkins home /var/lib/jenkins/.ansible_vault_pass

--- vars/mongoPipeline.groovy
+++ vars/mongoPipeline.groovy
@@ -38,7 +38,7 @@ def call() {
 
             stage('Playbook Execution') {
                 steps {
-                    sh "ansible-playbook -i ${env.CODE_BASE_PATH}/hosts.ini site.yml --vault-password-file ~/.ansible_vault_pass"
+                    sh "ansible-playbook -i ${env.CODE_BASE_PATH}/hosts.ini site.yml --vault-password-file /var/lib/jenkins/.ansible_vault_pass"
                 }
             }
 
